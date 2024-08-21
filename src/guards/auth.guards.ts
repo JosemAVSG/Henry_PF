f406b279ca4d4d640d750dcdc38e8f2eb8fb47ce
ricardoaot <ricardoaot@gmail.com>
2024-08-20 22:38:19 -0500

    Added auth guard implementation. Added verifyToken function in auth.controller.

---
+++ src/guards/auth.guards.ts
@@ -0,0 +1,36 @@
+import { CanActivate, Catch, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        private readonly jwtService: JwtService
+    ){}
+
+    canActivate(
+        context: ExecutionContext
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        
+        const request = context.switchToHttp().getRequest();
+
+        const token = request.headers['authorization']?.split(' ')[1] ?? '';
+        
+        if(!token){
+            throw new UnauthorizedException('Bearer Token not found')
+        }
+
+        try {
+            const secret = process.env.JWT_SECRET;
+            const payLoad = this.jwtService.verify(
+                token,
+                {secret}
+            )
+            request.user = payLoad
+            return true
+        }
+        catch(error){
+            throw new UnauthorizedException('Invalid Bearer Token')
+        }
+    }
+}
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../../guards/auth.guards';
+import { Request } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    @Get('/verifyToken')
+    @HttpCode(200)
+    @UseGuards(AuthGuard)
+    async verifyToken(@Req() req: Request){
+        console.log(req.user)
+        return true
+    }
+}
