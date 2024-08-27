4c43c2300958bc86b8e74d584895c303568392ad
ricardoaot <ricardoaot@gmail.com>
2024-08-27 17:45:32 -0500

    Fix create method in invoice service. Added endpoints in Ricardo's method folder.

--- src/modules/invoices/invoices.module.ts
+++ src/modules/invoices/invoices.module.ts
@@ -4,9 +4,10 @@ import { InvoicesController } from './invoices.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Invoice } from '../../entities/invoice.entity';
 import { InvoiceStatus } from '../../entities/invoiceStatus.entity';
+import { UserEntity } from '../../entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Invoice,InvoiceStatus])],
+  imports: [TypeOrmModule.forFeature([Invoice,InvoiceStatus, UserEntity])],
   providers: [InvoicesService],
   controllers: [InvoicesController]
 })
